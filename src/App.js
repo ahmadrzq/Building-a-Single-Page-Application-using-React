64a370f348efee100ca4597f87dd099c6c8d30b6
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-01-06 15:45:27 +0700

    Adding Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="app-container">
-      <h1>Hello, React</h1>
+      <header>
+        <h1><Link to="/">Aplikasi Catatan</Link></h1>
+        <nav className="navigation">
+          <ul>
+            <Navigation />
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 }
